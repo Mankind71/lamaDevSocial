409c36702983a84291b58d2d823cd2e6f4ac4702
Mankind71 <makindetolulope37@gmail.com>
2022-02-03 23:04:19 +0000

    design of a social page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+
+function App() {
+  // const classes = useStyles();
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
