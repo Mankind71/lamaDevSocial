c7c97e7481fcae2b5a5409ce99b243af3f167ff8
Mankind71 <makindetolulope37@gmail.com>
2022-02-04 21:48:00 +0000

    added pages with react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Register from "./pages/register/Register";
 
 function App() {
-  // const classes = useStyles();
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
